c049778f90e08daaec2e537e994e802e25b605ab
Carlos <servicioalcliente@johansen.com.do>
2025-08-07 13:07:03 -0400

    instalacion lista

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sgs/sistema-nomina',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f2c0936a13330477b43ec4e8ddce209ed38fbfd8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.10.0',
+            'version' => '6.10.0.0',
+            'reference' => 'bf74d75a1fde6beaa34a0ddae2ec5fce0f72a144',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'sgs/sistema-nomina' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f2c0936a13330477b43ec4e8ddce209ed38fbfd8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
